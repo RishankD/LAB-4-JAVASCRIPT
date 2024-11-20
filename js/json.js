0335f807ec317ce41884a9f10b28ac0b2a27004a
RishankD <156447323+RishankD@users.noreply.github.com>
2024-11-19 22:21:33 -0500

    Update json.js

--- js/json.js
+++ js/json.js
@@ -7,12 +7,11 @@ async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
      const requestURL = "./js/i-scream.json";
-    // STEP 5: Use the new URL to create a new request object
-    
-    // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
-    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const request = new Request(requestURL);
+  // STEP 6: Make a network request with the fetch() function, which returns a Response object
+  const response = await fetch(request);
+  // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+  const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
     
     // STEP 9a: Invoke the populateHeader function here, then build it below
